2dd09d83dc682c344787ff5d18c183747898fc20
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2026-02-10 17:47:43 -0600

    configuracion jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  }
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 };
+
+module.exports = createJestConfig(customJestConfig);
